b0380093cdbe693339f1dba4e536227ddfc5f8ad
mosta <mosta@Mostaghfera.lan>
2025-04-05 14:24:10 +0200

    ajout de( public void deconnexion() {} ) dans la class JeuClient de la Class Jeu.

--- UrbanMarginal-phase1/src/JeuClient.java
+++ UrbanMarginal-phase1/src/JeuClient.java
@@ -18,11 +18,18 @@ public class JeuClient extends Jeu {
 	public void reception() {
 	}
 	
+	
 	/**
 	 * Envoi d'une information vers le serveur
 	 * fais appel une fois à l'envoi dans la classe Jeu
 	 */
 	public void envoi() {
+	}
+
+	@Override
+	public void deconnexion() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
